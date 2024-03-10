243f0a502aff8e93d6339ac72bcd1e7b443a7ba1
Pespinho <pespinho@users.noreply.github.com>
2024-03-10 22:14:13 +0100

    feat: setup todo-comments

--- lua/user/plugins/todo-comments.lua
+++ lua/user/plugins/todo-comments.lua
@@ -6,4 +6,8 @@ local TodoComments = { "folke/todo-comments.nvim" }
 
 TodoComments.dependencies = { "nvim-lua/plenary.nvim" }
 
+TodoComments.config = function(_, opts)
+    require("todo-comments").setup(opts)
+end
+
 return TodoComments
