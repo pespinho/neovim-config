c7a5276b85663c832a2479bc886795aad4cb6858
Pespinho <pespinho@users.noreply.github.com>
2024-03-10 16:59:15 +0100

    feat: update keymaps for cmp (use tab for selecting and arrows for navigating)

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -29,29 +29,34 @@ local function border(hl_name)
     }
 end
 
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local function tab_keymap(fallback)
     local cmp = require "cmp"
     local luasnip = require "luasnip"
 
     if cmp.visible() then
-        cmp.confirm()
+        cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- that way you will only jump inside the snippet region
     elseif luasnip.expand_or_jumpable() then
-        local characters = vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true)
-        vim.fn.feedkeys(characters, "")
+        luasnip.expand_or_jump()
+    elseif has_words_before() then
+        cmp.complete()
     else
         fallback()
     end
 end
 
 local function shift_tab_keymap(fallback)
-    local cmp = require "cmp"
     local luasnip = require "luasnip"
 
-    if cmp.visible() then
-        cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
-        local characters = vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true)
-        vim.fn.feedkeys(characters, "")
+    if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
     else
         fallback()
     end
@@ -107,11 +112,12 @@ NvimCmp.opts = function()
         mapping = {
             ["<C-p>"] = cmp.mapping.select_prev_item(),
             ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<Up>"] = cmp.mapping.select_prev_item(),
+            ["<Down>"] = cmp.mapping.select_next_item(),
             ["<C-d>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-a>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
             ["<Tab>"] = cmp.mapping(tab_keymap, { "i", "s", }),
             ["<S-Tab>"] = cmp.mapping(shift_tab_keymap, { "i", "s", }),
         },
