a6a9b537449fdb82a0954086cb2995873d8bff63
Pespinho <pespinho@users.noreply.github.com>
2024-03-10 21:07:32 +0100

    feat: automatically close nvim-tree when it is the last window open

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -3,8 +3,25 @@
 -------------------------------------------------------------------------------
 
 local autocmds = {
-    vim_enter = function()
-        require("nvim-tree.api").tree.open()
+    quit_pre = function()
+        local tree_wins = {}
+        local floating_wins = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match("NvimTree_") ~= nil then
+                table.insert(tree_wins, w)
+            end
+            if vim.api.nvim_win_get_config(w).relative ~= '' then
+                table.insert(floating_wins, w)
+            end
+        end
+        if 1 == #wins - #floating_wins - #tree_wins then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(tree_wins) do
+                vim.api.nvim_win_close(w, true)
+            end
+        end
     end
 }
 
@@ -99,7 +116,10 @@ NvimTree.opts = {
 
 NvimTree.config = function(_, opts)
     require("nvim-tree").setup(opts)
-    -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = autocmds.vim_enter })
+
+    vim.api.nvim_create_autocmd("QuitPre", {
+        callback = autocmds.quit_pre
+    })
 end
 
 return NvimTree
