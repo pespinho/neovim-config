4d3d27fa8b6d5b0ee04e0e267ad6ba50abf7eb53
Pespinho <pespinho@users.noreply.github.com>
2024-04-15 13:15:44 +0200

    feat: add new keybinds for qf_helper + fix naming in which-key menu

--- lua/user/plugins/qf_helper.lua
+++ lua/user/plugins/qf_helper.lua
@@ -7,6 +7,9 @@ local QfHelper = { 'stevearc/qf_helper.nvim' }
 QfHelper.init = function()
     vim.keymap.set('n', '<leader>qd', '<Cmd>Reject<CR>', { desc = "[d]elete entry", silent = true })
     vim.keymap.set('n', '<leader>qc', '<Cmd>Cclear<CR>', { desc = "[c]lear", silent = true })
+    vim.keymap.set('n', '<leader>qt', '<Cmd>QFToggle!<CR>', { desc = "[t]oggle quickfix window", silent = true })
+    vim.keymap.set('n', '<leader>qn', '<Cmd>QFNext<CR>', { desc = "[n]ext", silent = true })
+    vim.keymap.set('n', '<leader>qp', '<Cmd>QFPrev<CR>', { desc = "[p]revious", silent = true })
 end
 
 QfHelper.opts = {
