0b2bf2c7e63d5fd9e85af61503908537a10114c3
Pespinho <pespinho@users.noreply.github.com>
2024-11-22 13:06:52 +0100

    feat: activate highlight on yank with Visual highlight group

--- lua/user/custom_commands.lua
+++ lua/user/custom_commands.lua
@@ -1,3 +1,14 @@
 vim.api.nvim_create_user_command("DiagnosticToggle", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end, { desc = "toggle diagnostic" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('user-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual' })
+    end,
+})
